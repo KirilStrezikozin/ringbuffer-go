ce5335f77626b711aa83c6ec6edd32c4effb70c2
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-03-31 00:21:51 +0200

    benchmarks/.*: add measure go-diodes, rename *Sync benchs into *Contended
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- benchmarks/go.mod
+++ benchmarks/go.mod
@@ -5,8 +5,9 @@ go 1.23.3
 replace github.com/KirilStrezikozin/ringbuffer-go => ../
 
 require (
+	code.cloudfoundry.org/go-diodes v0.0.0-20250324121313-75aea42a1fc3
 	github.com/KirilStrezikozin/ringbuffer-go v0.0.0-00010101000000-000000000000
 	github.com/hedzr/go-ringbuf/v2 v2.2.1
 )
 
-require golang.org/x/sys v0.30.0 // indirect
+require golang.org/x/sys v0.31.0 // indirect
--- benchmarks/ring_test.go
+++ benchmarks/ring_test.go
@@ -1,8 +1,12 @@
 package benchmarks
 
 import (
+	"crypto/rand"
+	"sync"
 	"testing"
+	"time"
 
+	"code.cloudfoundry.org/go-diodes"
 	"github.com/KirilStrezikozin/ringbuffer-go"
 	"github.com/hedzr/go-ringbuf/v2"
 )
@@ -12,7 +16,12 @@ type Message struct {
 	Text string
 }
 
-func BenchmarkGoChanSync(b *testing.B) {
+var (
+	diodeNopAlerter = diodes.AlertFunc(func(int) {})
+	randData        = randDataGen()
+)
+
+func BenchmarkGoChanContended(b *testing.B) {
 	ch := make(chan Message, 1)
 	msg := Message{ID: 1, Text: "hello world"}
 	var dummy Message
@@ -28,7 +37,7 @@ func BenchmarkGoChanSync(b *testing.B) {
 	}
 }
 
-func BenchmarkRingBufferKirilStrezikozinSync(b *testing.B) {
+func BenchmarkRingBufferKirilStrezikozinContended(b *testing.B) {
 	rb := ring.NewSync[Message](1)
 	msg := Message{ID: 1, Text: "hello world"}
 	var dummy Message
@@ -44,20 +53,175 @@ func BenchmarkRingBufferKirilStrezikozinSync(b *testing.B) {
 	}
 }
 
-func BenchmarkRingBufferHedzrSync(b *testing.B) {
+func BenchmarkRingBufferHedzrContended(b *testing.B) {
 	rb := ringbuf.New[Message](2)
 	msg := Message{ID: 1, Text: "hello world"}
-	var err error
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			err = rb.Enqueue(msg)
-			_, err = rb.Dequeue()
+			rb.Enqueue(msg) //nolint:errcheck
+			rb.Dequeue()    //nolint:errcheck
 		}
 	})
-	if err != nil {
-		b.Fail()
+}
+
+func BenchmarkDiodeContended(b *testing.B) {
+	d := diodes.NewWaiter(diodes.NewManyToOne(1000, diodeNopAlerter))
+	msg := Message{ID: 1, Text: "hello world"}
+    var dummy Message
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			d.Set(diodes.GenericDataType(&msg))
+			dummy = *(*Message)(d.Next())
+		}
+	})
+    if dummy != msg {
+        b.Fail()
+    }
+}
+
+func BenchmarkGoChanUncontended(b *testing.B) {
+	c := make(chan []byte, 100)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	done := make(chan struct{})
+	go func() {
+		wg.Done()
+		for {
+			select {
+			case <-c:
+			case <-done:
+				wg.Done()
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+	wg.Add(1)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		data := randData(i)
+		select {
+		case c <- *data:
+		default:
+			select {
+			case <-c:
+			default:
+			}
+		}
 	}
+
+	b.StopTimer()
+	close(done)
+	wg.Wait()
+}
+
+func BenchmarkRingBufferKirilStrezikozinUncontended(b *testing.B) {
+	rb := ring.NewSync[[]byte](100)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	done := make(chan struct{})
+	go func() {
+		wg.Done()
+		for {
+			select {
+			case <-done:
+				wg.Done()
+				return
+			default:
+				rb.Poll()
+				time.Sleep(time.Microsecond)
+			}
+		}
+	}()
+
+	wg.Wait()
+	wg.Add(1)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		data := randData(i)
+		rb.ForcePush(*data)
+	}
+
+	b.StopTimer()
+	close(done)
+	wg.Wait()
+}
+
+func BenchmarkRingBufferHedzrUncontended(b *testing.B) {
+	rb := ringbuf.New[[]byte](100)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	done := make(chan struct{})
+	go func() {
+		wg.Done()
+		for {
+			select {
+			case <-done:
+				wg.Done()
+				return
+			default:
+				rb.Dequeue() //nolint:errcheck
+				time.Sleep(time.Microsecond)
+			}
+		}
+	}()
+
+	wg.Wait()
+	wg.Add(1)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		data := randData(i)
+		rb.Enqueue(*data) //nolint:errcheck
+	}
+
+	b.StopTimer()
+	close(done)
+	wg.Wait()
+}
+
+func BenchmarkDiodeUncontended(b *testing.B) {
+	d := diodes.NewPoller(diodes.NewOneToOne(100, diodeNopAlerter))
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	done := make(chan struct{})
+	go func() {
+		wg.Done()
+		for {
+			select {
+			case <-done:
+				wg.Done()
+				return
+			default:
+				d.TryNext()
+				time.Sleep(time.Microsecond)
+			}
+		}
+	}()
+
+	wg.Wait()
+	wg.Add(1)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		data := randData(i)
+		d.Set(diodes.GenericDataType(data))
+	}
+
+	b.StopTimer()
+	close(done)
+	wg.Wait()
 }
 
 func BenchmarkGoChanCreation(b *testing.B) {
@@ -94,4 +258,32 @@ func BenchmarkRingBufferHedzrCreation(b *testing.B) {
 			}
 		}
 	})
+}
+
+func BenchmarkDiodeCreation(b *testing.B) {
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			d := diodes.NewOneToOne(1, diodeNopAlerter)
+			if _, ok := d.TryNext(); ok {
+				b.Fail()
+			}
+		}
+	})
+}
+
+func randDataGen() func(int) *[]byte {
+	// https://github.com/cloudfoundry/go-diodes/blob/main/benchmarks_test.go
+
+	var data [][]byte
+
+	for j := 0; j < 5; j++ {
+		buffer := make([]byte, 100)
+		rand.Read(buffer) //nolint:errcheck
+		data = append(data, buffer)
+	}
+
+	return func(i int) *[]byte {
+		return &data[i%len(data)]
+	}
 }
